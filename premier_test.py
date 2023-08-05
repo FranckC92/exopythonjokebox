dc28a040346d43f1b9aa6ebd2fa4d45d6c3d02bb
ceyssat <ceyssat.franck@gmail.com>
2023-08-05 19:30:34 +0200

    1er commit

---
+++ premier_test.py
@@ -0,0 +1,17 @@
+""" Fichier: base.py"""
+
+import os, os.path
+import sys
+
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+def main(args=sys.argv[:1]):
+    """ Main"""
+    print("Usage: %s" % os.path.basename(sys.argv[0]))
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
+    
